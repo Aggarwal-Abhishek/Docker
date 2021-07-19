e4c039a0764b8149ebac77ae84a6d82e2dd76c50
Abhishek Aggarwal <13921008+Aggarwal-Abhishek@users.noreply.github.com>
2021-07-19 15:25:08 +0530

    Update DatabaseHandler.py
    Retry until Database is connected

--- WebNotes/api/DatabaseHandler.py
+++ WebNotes/api/DatabaseHandler.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 import psycopg2
 
 def GetNotes(username):
@@ -43,18 +43,20 @@ def CreateTable():
     
     conn.commit()
 
-try:
-    conn = psycopg2.connect(
-        host = os.environ['DB_HOST'],
-        database = os.environ['DB_NAME'],
-        user = os.environ['DB_USERNAME'],
-        password = os.environ['DB_PASSWORD']
-    )
-    cursor = conn.cursor()
-
-except Exception as error:
-    print("Connecting to database failed...")
-    print("Error:", error)
-    exit(0)
-
-
+while True:
+    try:
+        conn = psycopg2.connect(
+            host = os.environ['DB_HOST'],
+            database = os.environ['DB_NAME'],
+            user = os.environ['DB_USERNAME'],
+            password = os.environ['DB_PASSWORD']
+        )
+        cursor = conn.cursor()
+        print("Database Connected Successifully :-)")
+        break
+        
+    except Exception as error:
+        print("Connecting to database failed...Trying Again...")
+        print("Error:", error)
+        time.sleep(2)
+        
